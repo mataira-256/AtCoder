1682ca7d807c9e505bbb3dc6d0ef5fc089f1a93f
mataira-256 <128944427+mataira-256@users.noreply.github.com>
2024-04-25 12:13:39 +0900

    AOJ complete

--- study/初級編/AOJ/10_D.go
+++ study/初級編/AOJ/10_D.go
@@ -1,17 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	var n int
-	var x [105]int
-	var y [105]int
+	var x [105]float64
+	var y [105]float64
+	var p1, p2, p3, pInf float64
 
 	fmt.Scanf("%d\n", &n)
 	for i := 0; i < n; i++ {
-		fmt.Scanf("%d", &x[i])
+		fmt.Scanf("%f", &x[i])
 	}
 	for i := 0; i < n; i++ {
-		fmt.Scanf("%d", &y[i])
+		fmt.Scanf("%f", &y[i])
 	}
+
+	pInf = 0
+	for i := 0; i < n; i++ {
+		diff := math.Abs(x[i] - y[i])
+		p1 += diff
+		p2 += math.Pow(diff, 2)
+		p3 += math.Pow(diff, 3)
+		if pInf < diff {
+			pInf = diff
+		}
+	}
+	p2 = math.Sqrt(p2)
+	p3 = math.Cbrt(p3)
+
+	fmt.Printf("%f\n", p1)
+	fmt.Printf("%f\n", p2)
+	fmt.Printf("%f\n", p3)
+	fmt.Printf("%f\n", pInf)
 }
