2310942e120d3e568b3947054ccc3f629525f140
mataira-256 <128944427+mataira-256@users.noreply.github.com>
2024-05-13 17:57:03 +0900

    a

---
+++ study/past/2024_05_13/ABC349C.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var (
+		S string
+		T string
+	)
+	var ans bool = false
+
+	fmt.Scanf("%s\n", &S)
+	fmt.Scanf("%s\n", &T)
+
+	S = strings.ToUpper(S)
+
+	T1index := strings.Index(S, string(T[0]))
+	if T1index != -1 {
+		T2index := strings.Index(S[T1index+2:], string(T[1]))
+		if T2index != -1 {
+			if T[2] == 'X' { // Tの3文字目がXならこの時点でok
+				ans = true
+			}
+			if strings.Index(S[T2index+2:], string(T[2])) != -1 {
+				ans = true
+			}
+		}
+	}
+
+	if ans {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
