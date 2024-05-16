226636825723175186d0d9fbf7ed60264d961cd0
mataira-256 <128944427+mataira-256@users.noreply.github.com>
2024-05-16 16:42:10 +0900

    入力が一番つらい

--- study/past/2024_05_15/ABC294D.go
+++ study/past/2024_05_15/ABC294D.go
@@ -6,20 +6,39 @@ func main() {
 	var (
 		N     int
 		Q     int
-		ibent [500005][2]int
+		event [500005][2]int
 	)
+	var last int = 1
+	var min int
+	var called [500005]int
+	var count3 int
+	var ans [500005]int
 
 	fmt.Scanf("%d %d\n", &N, &Q)
 	for i := 0; i < Q; i++ {
-		fmt.Scanf("%d", &ibent[i][0])
-		ibent[i][1] = -1
-		if ibent[i][0] == 2 {
-			fmt.Scanf("%d", &ibent[i][1])
+		fmt.Scanf("%d", &event[i][0])
+		if event[i][0] == 2 {
+			fmt.Scanf("%d", &event[i][1])
 		}
-		fmt.Scanln()
 	}
 
 	for i := 0; i < Q; i++ {
-		fmt.Println(ibent[i])
+		switch event[i][0] {
+		case 1:
+			called[last-1] = last
+			last++
+		case 2:
+			called[event[i][1]-1] = 0
+		case 3:
+			for called[min] == 0 {
+				min++
+			}
+			ans[count3] = called[min]
+			count3++
+		}
+	}
+
+	for i := 0; i < count3; i++ {
+		fmt.Println(ans[i])
 	}
 }
