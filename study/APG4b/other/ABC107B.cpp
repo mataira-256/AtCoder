dc4bb3be6cafb68e9041ef6a9d319683236d2107
mataira-256 <128944427+mataira-256@users.noreply.github.com>
2024-05-23 18:33:17 +0900

    a

---
+++ study/APG4b/other/ABC107B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+    int H, W;
+
+    cin >> H >> W;
+
+    vector<vector<char>> grid(H, vector<char>(W));
+
+    rep(i, H) rep(j, W) cin >> grid.at(i).at(j);
+
+
+}
