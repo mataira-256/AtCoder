53b3ecc1868dbd202c67a2e8475f0a58637947ed
mataira-256 <128944427+mataira-256@users.noreply.github.com>
2024-06-02 23:54:40 +0900

    a

---
+++ study/other/2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <atcoder/dsu>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+
+int main() {
+    vector<int> x;
+
+    x.push_back(1);
+
+    rep(i, x.size()) cout << x.at(i) << " ";
+}
